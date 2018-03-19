315f184f6b2f497da18913fd21fc2609b80129c3
nuits.jp <nuits.jp@live.jp>
2018-03-19 21:00:17 +0900

    Add Project.

---
+++ XFormsApp/XFormsApp/MainPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XFormsApp
+{
+    public class MainPageViewModel
+    {
+        public ICommand InitializeCommand => new Command(() => { });
+    }
+}
---
+++ XFormsApp/XFormsApp/PageBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XFormsApp
+{
+    public static class PageBehavior
+    {
+        public static readonly BindableProperty AppearingProperty =
+            BindableProperty.CreateAttached("Appearing", typeof(ICommand), typeof(PageBehavior), null, propertyChanged: OnAppearingChanged);
+
+        public static ICommand GetAppearing(BindableObject bindableObject)
+        {
+            return (ICommand)bindableObject.GetValue(AppearingProperty);
+        }
+
+        private static void OnAppearingChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is Page target)
+            {
+                if (oldValue == null && newValue != null)
+                {
+                    target.Appearing += OnAppearing;
+                }
+                else if (oldValue != null && newValue == null)
+                {
+                    target.Appearing -= OnAppearing;
+                }
+            }
+        }
+
+        private static void OnAppearing(object o, EventArgs eventArgs)
+        {
+            var command = GetAppearing((BindableObject)o);
+            if (command.CanExecute(eventArgs))
+                command.Execute(eventArgs);
+        }
+    }
+}
